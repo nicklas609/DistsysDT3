d1d5b92e5db3b30633aec89e2262d0f21382e06e
Marcus Kofoed Kirkegaard <mkki@itu.dk>
2023-10-23 12:28:23 +0200

    Added GRPC template

---
+++ Mandatory3/Server/Server.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+
+	proto "github.com/MarcusKofoedKirkegaard/gRPC-Exercises.git/proto"
+
+	"strconv"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+// Struct that will be used to represent the Server.
+type Server struct {
+	proto.UnimplementedTemplateServer // Necessary
+	name                              string
+	port                              int
+}
+
+// Used to get the user-defined port for the server from the command line
+var port = flag.Int("port", 0, "server port number")
+
+func main() {
+	// Get the port from the command line when the server is run
+	flag.Parse()
+
+	// Create a server struct
+	server := &Server{
+		name: "serverName",
+		port: *port,
+	}
+
+	// Start the server
+	go startServer(server)
+
+	// Keep the server running until it is manually quit
+	for {
+
+	}
+}
+
+func startServer(server *Server) {
+
+	// Create a new grpc server
+	grpcServer := grpc.NewServer()
+
+	// Make the server listen at the given port (convert int port to string)
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(server.port))
+
+	if err != nil {
+		log.Fatalf("Could not create the server %v", err)
+	}
+	log.Printf("Started server at port: %d\n", server.port)
+
+	// Register the grpc server and serve its listener
+	proto.RegisterFarewellServer(grpcServer, server)
+	serveError := grpcServer.Serve(listener)
+	if serveError != nil {
+		log.Fatalf("Could not serve listener")
+	}
+}
+
+func (c *Server) AskForTime(ctx context.Context, in *proto.AskForTimeMessage) (*proto.TimeMessage, error) {
+	log.Printf("Client with ID %d asked for the time\n", in.ClientId)
+	return &proto.TimeMessage{
+		Time:       time.Now().String(),
+		ServerName: c.name,
+	}, nil
+}
